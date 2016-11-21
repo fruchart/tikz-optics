0d453e63441fe25ad18fce35742f0130692835b5
Michel Fruchart <michel.fruchart@ens-lyon.org>
2016-11-21 02:08:23 +0100

    Scripts to create TDS/CTAN packages.

---
+++ build_ctan.sh
@@ -0,0 +1,38 @@
+#!/bin/bash    
+# Script to create a CTAN package for tikz-optics.
+
+# create a temporary directory (to put the contents of the zip file to be)
+# and register a cleanup function to remove it at EXIT
+readonly tmpdir=$(mktemp -d)
+function cleanup {
+  rm -r "$tmpdir"
+}
+trap cleanup EXIT
+
+# create a folder (to stay in the zip) and put our files in it
+ctandir="$tmpdir/tikz-optics/"
+mkdir -p "$ctandir"
+cp README.md -t "$ctandir"
+cp tikzlibraryoptics.code.tex -t "$ctandir"
+cp doc/tikz-optics.tex -t "$ctandir"
+cp doc/tikz-optics.pdf -t "$ctandir"
+# if a TDS-structured archive exists, we include it
+if [ -f "tikz-optics.tds.zip" ]; then
+	cp "tikz-optics.tds.zip" -t "$ctandir"
+else
+	echo -e "\e[1m\e[91mWarning:\e[39m\e[21m no tikz-optics.tds.zip file found so it will not be included; use build_tds.sh to create/update it."
+fi
+
+
+# zip the contents into the CTAN package
+outfile="tikz-optics.zip"
+if [ -e outfile ]; then
+	rm "$outfile"
+fi;
+pushd "$tmpdir" > /dev/null
+zip -qr "$outfile" ./
+popd > /dev/null
+mv "$tmpdir/$outfile" -t ./
+
+echo "CTAN package created: $outfile"
+exit 0
---
+++ build_tds.sh
@@ -0,0 +1,32 @@
+#!/bin/bash    
+# Script to create a TDS-structured package for tikz-optics.
+# See https://www.ctan.org/TDS-guidelines for the TDS structure.
+
+# create a temporary directory (to put the contents of the zip file to be)
+# and register a cleanup function to remove it at EXIT
+readonly tmpdir=$(mktemp -d)
+function cleanup {
+  rm -r "$tmpdir"
+}
+trap cleanup EXIT
+
+# create the required TDS structure and put our files in it
+docdir="$tmpdir/doc/latex/tikz-optics/"
+texdir="$tmpdir/tex/latex/tikz-optics/"
+mkdir -p "$docdir"
+mkdir -p "$texdir"
+cp doc/tikz-optics.pdf -t "$docdir"
+cp tikzlibraryoptics.code.tex -t "$texdir"
+
+# zip the contents into the wanted TDS package
+outfile="tikz-optics.tds.zip"
+if [ -e outfile ]; then
+	rm "$outfile"
+fi;
+pushd "$tmpdir" > /dev/null
+zip -qr "$outfile" ./
+popd > /dev/null
+mv "$tmpdir/$outfile" -t ./
+
+echo "TDS package created: $outfile"
+exit 0
